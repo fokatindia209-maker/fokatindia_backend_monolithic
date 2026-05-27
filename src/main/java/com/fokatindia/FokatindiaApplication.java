907bf64a1decffb2bb3af8d4dab06dd7d0f82e4f
myvastuastro <myvastuastro@gmail.com>
2026-05-28 02:20:31 +0400

    new

---
+++ src/main/java/com/fokatindia/FokatindiaApplication.java
@@ -0,0 +1,13 @@
+package com.fokatindia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FokatindiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FokatindiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fokatindia/FokatindiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fokatindia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FokatindiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
