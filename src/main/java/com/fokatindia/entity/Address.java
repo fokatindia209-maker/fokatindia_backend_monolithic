ada0d73cd409eab14716f1107e35c14ce3e2a25c
myvastuastro <myvastuastro@gmail.com>
2026-06-20 14:49:02 +0400

    fdfsd

--- src/main/java/com/fokatindia/entity/Address.java
+++ src/main/java/com/fokatindia/entity/Address.java
@@ -2,6 +2,7 @@ package com.fokatindia.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
@@ -11,27 +12,39 @@ import java.time.LocalDateTime;
 public class Address {
 
     @Id
+    @Column("address_id")
     private Long addressId;
 
+    @Column("user_id")
     private Long userId;
 
+    @Column("address_line1")
     private String addressLine1;
 
+    @Column("address_line2")
     private String addressLine2;
 
+    @Column("city")
     private String city;
 
+    @Column("state")
     private String state;
 
+    @Column("pincode")
     private String pincode;
 
+    @Column("country")
     private String country;
 
+    @Column("latitude")
     private Double latitude;
 
+    @Column("longitude")
     private Double longitude;
 
+    @Column("is_default")
     private Boolean isDefault;
 
+    @Column("created_at")
     private LocalDateTime createdAt;
 }
